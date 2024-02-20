71645d1a1a4bb5c93da25a7276d9e2c56bebbf8b
baijid Rahman <baijidrahman12@gmail.com>
2024-02-20 13:23:46 +0000

    Add urls.py file in blog folder

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -22,7 +22,7 @@ if os.path.isfile("env.py"):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-
+#{% url 'post_detail' post.slug  %}
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
